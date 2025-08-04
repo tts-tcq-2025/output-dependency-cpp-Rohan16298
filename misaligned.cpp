05733e85bf2a11d7e47c8f2110ecc3516bef9473
Venugopal K <venu.shastri@gmail.com>
2025-08-04 10:53:36 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -36,17 +36,4 @@ void printColorMap(const std::vector<ColorMapEntry>& colorMap) {
     }
 }
 
-// Test function to verify the generation of the color map
-void testGenerateColorMap() {
-    std::vector<ColorMapEntry> colorMap = generateColorMap();
-    assert(colorMap.size() == 25);
-    // Additional tests can be added here to verify content correctness
-    std::cout << "Color map generation test passed.\n";
-}
 
-int main() {
-    testGenerateColorMap();
-    std::vector<ColorMapEntry> colorMap = generateColorMap();
-    printColorMap(colorMap);
-    return 0;
-}
